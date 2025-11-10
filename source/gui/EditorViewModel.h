f144d4f37ee20f5e494bd49f7e7d0315ffc395d6
Corredor1230 <juanpablocorredor97@gmail.com>
2025-11-10 13:57:31 +0900

    New file to act as bridge between GUI and DSP. This will make it easier to integrate code with JUCE, Qt and other GUI libraries

---
+++ source/gui/EditorViewModel.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "include/SitranoHeader.h" 
+#include "file/SihatFile.h"   
+#include <string>
+#include <atomic>     // For the isProcessing flag
+#include <functional> // For the callbacks
+
+struct EditorViewModel
+{
+    // The GUI will read/write these values directly.
+    Sitrano::AnalysisConfig config;
+    Sitrano::Settings       settings;
+    SihatFile::OutInfo      info;
+    std::string             jsonPath;
+
+    // The GUI reads this to disable buttons, show spinners, etc.
+    std::atomic<bool> isProcessing{ false };
+
+    // The GUI will call these functions when buttons are clicked.
+    // Our 'main.cpp' will provide the definitions for these.
+    std::function<void(void)> onSaveRequested;
+    std::function<void(void)> onRunSingleRequested;
+    std::function<void(void)> onRunBulkRequested;
+};
