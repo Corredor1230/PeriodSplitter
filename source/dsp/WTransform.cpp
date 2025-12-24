4e63b529cff0a2d82dc5640ab890fc73809eb0d5
Corredor1230 <juanpablocorredor97@gmail.com>
2025-12-24 16:42:22 +0900

    To be deleted

--- source/dsp/WTransform.cpp
+++ source/dsp/WTransform.cpp
@@ -1,28 +1,28 @@
-#include"WTransform.h"
+// #include"WTransform.h"
 
-WTransform::WTransform(int windowSize)
-: wSize(windowSize)
-{
-    plan, input, output = nullptr;
+// WTransform::WTransform(int windowSize)
+// : wSize(windowSize)
+// {
+//     plan, input, output = nullptr;
 
-    initFFTW();
-}
+//     initFFTW();
+// }
 
-WTransform::~WTransform()
-{
-    if (plan != nullptr) fftwf_destroy_plan(plan);
-    if (input != nullptr) fftwf_free(input);
-    if (output != nullptr) fftwf_free(output);
-}
+// WTransform::~WTransform()
+// {
+//     if (plan != nullptr) fftwf_destroy_plan(plan);
+//     if (input != nullptr) fftwf_free(input);
+//     if (output != nullptr) fftwf_free(output);
+// }
 
-void WTransform::initFFTW()
-{
-    input = (float*)fftwf_malloc(sizeof(float) * wSize);
-    output = (fftwf_complex*)fftwf_malloc(sizeof(float) * wSize / 2 + 1);
-    plan = fftwf_plan_dft_r2c_1d(wSize, input, output, FFTW_MEASURE);
-}
+// void WTransform::initFFTW()
+// {
+//     input = (float*)fftwf_malloc(sizeof(float) * wSize);
+//     output = (fftwf_complex*)fftwf_malloc(sizeof(float) * wSize / 2 + 1);
+//     plan = fftwf_plan_dft_r2c_1d(wSize, input, output, FFTW_MEASURE);
+// }
 
-std::vector<float> WTransform::wave(std::vector<float>& input, int windowSize)
-{
+// std::vector<float> WTransform::wave(std::vector<float>& input, int windowSize)
+// {
 
-}
+// }
