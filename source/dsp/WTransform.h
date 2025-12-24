6b8208e8e90c8561f97478398176926e0111bf29
Corredor1230 <juanpablocorredor97@gmail.com>
2025-12-24 16:42:10 +0900

    Commented out. Deprecated file. Will remove

--- source/dsp/WTransform.h
+++ source/dsp/WTransform.h
@@ -1,22 +1,50 @@
 #pragma once
 #include"include/SitranoHeader.h"
 #include<vector>
+#include<complex>
 #include<fftw3.h>
 
 class WTransform
 {
 public:
-    WTransform(int windowSize);
-    ~WTransform();
+    WTransform(std::vector<float> input, float sampleRate)
+    : signal(input), fs(sampleRate)
+    {
+        nfft = Sitrano::findNextPowerOfTwo(signal.size());
+        signal.resize(nfft, 0.f);
+        initFFTW();
+    }
 
-private:
-    const int wSize;
-    float* input;
-    fftwf_complex* output;
-    fftwf_plan plan;
+    virtual ~WTransform()
+    {
+        fftwf_destroy_plan(fwdPlan);
+        fftwf_destroy_plan(invPlan);
+        fftwf_free(inBuffer);
+        fftwf_free(fDomainBuffer);
+        fftwf_free(invBuffer);
+    };
 
-    std::vector<float> wave(std::vector<float>& input, int windowSize);
+    virtual std::vector<float> process(const std::vector<float>& frequencies) = 0;
 
-    void initFFTW();
+protected:
+    float* inBuffer;
+    fftwf_complex* fDomainBuffer;
+    fftwf_complex* invBuffer;
+    fftwf_plan fwdPlan;
+    fftwf_plan invPlan;
 
+    std::vector<float> signal;
+    int nfft = 0;
+    const float fs;
+
+    void initFFTW()
+    {
+        inBuffer = (float*)fftwf_malloc(sizeof (float) * nfft);
+        fDomainBuffer = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * (nfft / 2 + 1));
+        invBuffer = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * nfft);
+        fwdPlan = fftwf_plan_dft_r2c_1d(nfft, inBuffer, fDomainBuffer, FFTW_MEASURE);
+        invPlan = fftwf_plan_dft_1d(nfft, invBuffer, invBuffer, FFTW_BACKWARD, FFTW_MEASURE);
+    }
+    
+    virtual std::vector<float> wavelet(float freq) = 0;
 };
