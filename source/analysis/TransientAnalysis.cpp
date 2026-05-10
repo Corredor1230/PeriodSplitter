9cd7708816b9266ca11d78fa12c24c28fcac05ae
Corredor1230 <juanpablocorredor97@gmail.com>
2026-05-10 22:43:00 +0900

    Removed unnecessary comments

--- source/analysis/TransientAnalysis.cpp
+++ source/analysis/TransientAnalysis.cpp
@@ -95,53 +95,6 @@ Sihat::SampleRange TransientAnalysis::findBoundaries(const std::vector<float>& i
     outRange.endSample = Sihat::findNextZero(input, appEndSamp);
 
     return outRange;
-
-    // std::vector<float> rmsList;
-    // bool transientFound = false;
-    // int tInitSample = 0;
-    // int tEndSample = 0;
-    // float rmsPeak = 0.f;
-    // int numBelowPeak = 0;
-
-    // std::vector<float> rmsWindow(settings.rmsWindow); // This will hold the transient
-    // bool audioDetected = false;
-
-    // for (int samp = settings.startSample; samp < (input.size() / 4); samp += settings.rmsHopSize)
-    // {
-    //     if (input[samp] == 0.f && !audioDetected) continue;
-    //     else if (input[samp] != 0.f && !audioDetected) audioDetected = true;
-        
-    //     float windowSum = 0.f;
-
-    //     for (int rmsSamp = 0; rmsSamp < settings.rmsWindow && (rmsSamp + samp) < input.size(); rmsSamp++)
-    //     {
-    //         float x = input[samp + rmsSamp];
-    //         windowSum += (x * x);
-    //         rmsWindow[rmsSamp] = x;
-    //     }
-
-    //     float rms = std::sqrt(windowSum / settings.rmsWindow);
-    //     if (rms > rmsPeak) rmsPeak = rms;
-    //     rmsList.push_back(rms);
-    // }
-
-    // std::transform(rmsList.begin(), rmsList.end(), rmsList.begin(), [rmsPeak](float f){return f / rmsPeak;});
-
-    // for (int i = 0; i < rmsList.size(); i++)
-    // {
-    //     float currVal = rmsList[i];
-    // }
-
-    // if (!transientFound) {
-    //     // No transient, just find the first peak in a small window
-    //     int peakSample = Sitrano::findPeakSample(input, 0, std::min((int)input.size(), 8192), true);
-    //     range.endSample = Sitrano::findPreviousZero(input, peakSample);
-    //     return range;
-    // }
-
-    // range.initSample = Sitrano::findPreviousZero(input, tInitSample);
-    // range.endSample = Sitrano::findPreviousZero(input, tEndSample);
-    // return range;
 }
 
 Sihat::Spectrogram TransientAnalysis::computeSpectrogram(const std::vector<float>& audio, const Sihat::SampleRange& range){
